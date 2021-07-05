d2d197adfc743f27a0b434d9ff3de9ca5c13ee20
bjaud <l1ght3rth4nl1ght@gmail.com>
2021-07-05 15:22:43 +0200

    Update boilerplate to fit typescript support

--- index.ts
+++ index.ts
@@ -1,10 +1,21 @@
-import express from 'express';
+import express from "express";
 
-import morgan from './utils/morganMiddleware';
-import logger from './utils/logger';
+import morganMiddleware from "./src/utils/morganMiddleware";
+import logger from "./src/utils/logger";
 
 const app = express();
-app.use(morgan);
+app.use(morganMiddleware);
 
-const PORT = 3000;
-app.listen(PORT, () => logger.info(`Server running on port:${PORT}...`));
+const port = 3000;
+
+// example batch of logger types
+app.get("/logger", (_, res) => {
+    logger.error("This is an error log");
+    logger.warn("This is a warn log");
+    logger.info("This is a info log");
+    logger.http("This is a http log");
+    logger.debug("This is a debug log");
+    res.send("Hello world");
+});
+
+app.listen(port, () => logger.info(`Server running on port:${port}...`));
