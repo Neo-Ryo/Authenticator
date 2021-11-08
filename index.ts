326e890172e0687aa55e09fe613c18d045e21303
beluga <benjamin.jaud@neomanis.io>
2021-11-08 10:20:25 +0100

    :heavy_plus_sign: add manager for cors(), update tsconfig

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
 import { app } from "./server";
 import logger from "./src/utils/logger";
 
-app.listen(process.env.PORT, () => logger.info(`Server running on port:${process.env.PORT}...`));
+app.listen(process.env.LOCAL_PORT, () => logger.info(`Server running on port:${process.env.LOCAL_PORT}...`));
--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
-import express, { Request, Response, NextFunction } from "express";
+import express from "express";
 import morganMiddleware from "./src/utils/morganMiddleware";
 import helmet from "helmet";
+import cors from "cors";
 import dotenv from "dotenv";
 import swaggerUi from "swagger-ui-express";
 import * as swaggerDoc from "./src/swagger/swagger.json";
@@ -18,15 +19,7 @@ app.use(
     })
 );
 app.use(express.json());
-app.use((req: Request, res: Response, next: NextFunction) => {
-    res.setHeader("Access-Control-Allow-Origin", `${process.env.FRONT_URL}`);
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-    );
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
-    next();
-});
+app.use(cors());
 
 app.use("/apiDocs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.use("/status", statusRouter);
