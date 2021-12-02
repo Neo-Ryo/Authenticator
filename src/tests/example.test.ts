9d655ecbcd378be61ec23b8d5609a2c3e3c435de
tardigrade <benjamin.jaud@neomanis.io>
2021-12-02 10:45:11 +0100

    :white_check_mark: add test for logger

--- src/tests/example.test.ts
+++
@@ -1,9 +0,0 @@
-// This file can be deleted as soon as other tests have been written
-describe("Test example", () => {
-    test("it should be equal", () => {
-        expect(("b" + "a" + +"a" + "a").toLowerCase()).toEqual("banana");
-    });
-    test("it should not be equal", () => {
-        expect(true).not.toEqual(![]);
-    });
-});
