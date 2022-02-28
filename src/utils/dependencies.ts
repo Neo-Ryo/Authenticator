44223eca91d2bdb530f06ec363870581a34ee654
tardigrade <benjamin.jaud@neomanis.io>
2022-02-28 14:25:21 +0100

    :alien: connect to register service

---
+++ src/utils/dependencies.ts
@@ -0,0 +1,8 @@
+import { IServiceUrl, ServiceName } from "@neomanis/neo-utilities";
+
+export const requiredDependencies: ServiceName[] = [];
+export const otherDependencies: ServiceName[] = [];
+
+export const activeRequiredDependencies: IServiceUrl[] = [];
+
+export const activeOtherDependencies: IServiceUrl[] = [];
