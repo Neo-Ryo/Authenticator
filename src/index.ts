052c5559b64db40f3ff189387e4d1850e44ae954
mschiavone <marc.schiavone@neomanis.io>
2022-12-16 15:12:59 +0100

    remove useless files

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { app } from "./server";
-import logger from "./utils/logger";
 import { config } from "dotenv";
 import { User } from "./entities/User";
 import { sequelizeInstance } from "./databaseInit";
@@ -13,10 +12,10 @@ const { LOCAL_PORT, DB_NAME } = process.env;
         await sequelizeInstance.authenticate();
         // await User.sync({ force: true });
         app.listen(LOCAL_PORT || 9000, () => {
-            logger.info(`Database ${DB_NAME}, connected`);
-            logger.info(`Server running on port: ${LOCAL_PORT}`);
+            console.log(`Database ${DB_NAME}, connected`);
+            console.log(`Server running on port: ${LOCAL_PORT}`);
         });
     } catch (error) {
-        logger.error(error);
+        console.error(error);
     }
 })();
